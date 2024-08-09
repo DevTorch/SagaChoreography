01f02856e2c36afb096b8f531252c68562a27e93
Vitaliy <torchez@yandex.ru>
2024-08-09 13:45:32 +0300

    Basic multi-module project configuration

---
+++ order-service/src/main/java/com/github/devtorch/saga/orderservice/model/mapper/DependenciesTestMapper.java
@@ -0,0 +1,7 @@
+package com.github.devtorch.saga.orderservice.model.mapper;
+
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface DependenciesTestMapper {
+}
--- order-service/src/test/java/com/github/devtorch/saga/orderservice/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/github/devtorch/saga/orderservice/OrderServiceApplicationTests.java
@@ -1,13 +1,8 @@
 package com.github.devtorch.saga.orderservice;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class OrderServiceApplicationTests {
-
-//    @Test
-//    void contextLoads() {
-//    }
 
 }
