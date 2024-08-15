cc5921231e5c54ad564229a2bb6f52a55b9f3062
Vitaliy <torchez@yandex.ru>
2024-08-15 18:45:31 +0300

    Add few classes

--- order-service/src/main/java/com/github/devtorch/saga/orderservice/domain/OrderItem.java
+++ order-service/src/main/java/com/github/devtorch/saga/orderservice/domain/OrderItem.java
@@ -1,7 +1,16 @@
 package com.github.devtorch.saga.orderservice.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.math.BigDecimal;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderItem {
     private Long id;
     private Long productStockId;
