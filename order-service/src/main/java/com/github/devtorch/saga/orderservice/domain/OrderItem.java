ffc7e8ba029d7f60ee67693acd94ad41b10d5b96
Vitaliy <torchez@yandex.ru>
2024-09-09 16:07:29 +0300

    Update Entities

--- order-service/src/main/java/com/github/devtorch/saga/orderservice/domain/OrderItem.java
+++ order-service/src/main/java/com/github/devtorch/saga/orderservice/domain/OrderItem.java
@@ -1,21 +1,51 @@
 package com.github.devtorch.saga.orderservice.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.proxy.HibernateProxy;
 
 import java.math.BigDecimal;
+import java.util.List;
+import java.util.Objects;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private Long id;
     private Long productStockId;
     private String productTitle;
     private Integer quantity;
     private BigDecimal price;
+    @ManyToMany
+    @JoinTable(name = "orders_orderitems")
+    private List<Order> orders;
 
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        OrderItem orderItem = (OrderItem) o;
+        return getId() != null && Objects.equals(getId(), orderItem.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
 }
